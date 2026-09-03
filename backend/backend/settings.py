30aaed5988b7b382a614441e75345d00409432a9
Mauroo <maurvyt@gmail.com>
2026-09-03 12:16:11 -0300

    act static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,8 +80,7 @@ TEMPLATES = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '..', 'frontend', 'dist'), # Directorio base
-    os.path.join(BASE_DIR, '..', 'frontend', 'dist', 'assets'), # Directorio de assets
+    os.path.join(BASE_DIR, '..', 'frontend', 'dist'),
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
