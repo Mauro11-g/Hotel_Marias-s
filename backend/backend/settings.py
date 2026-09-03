8d2ef7b5fe1471994c5fc6647e50728fd4af057b
Mauroo <maurvyt@gmail.com>
2026-09-03 15:14:26 -0300

    Correciones

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,6 +20,11 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+FRONTEND_DIST_DIR = BASE_DIR / 'frontend' / 'dist'
+
+STATICFILES_DIRS = [
+    FRONTEND_DIST_DIR,
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -32,7 +37,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 DJANGO_VITE_DEV_MODE = os.environ.get('DJANGO_VITE_DEV_MODE', 'False') == 'True'
 
-DJANGO_VITE_ASSETS_PATH = BASE_DIR.parent / 'frontend' / 'dist'
+DJANGO_VITE_ASSETS_PATH = FRONTEND_DIST_DIR
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
@@ -83,9 +88,6 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend' / 'dist',
-]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
